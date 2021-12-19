f065c229228d1cd6309d6f08774d082afdffbc76
unknown <m.kucha@wp.pl>
2021-12-19 21:07:51 +0100

    initial commit

---
+++ classify.py
@@ -0,0 +1,46 @@
+﻿import os
+# Disable TF warning messages
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import tensorflow as tf
+import numpy as np
+import cv2     # pip install opencv-python
+
+# Directory with test set
+TEST_DATASET_DIR = 'mnist-test'
+
+# Trained model name
+MODEL = 'model.h5'
+
+# Load trained model of neural network
+model = tf.keras.models.load_model(MODEL)
+(_, _), (x_test, _) = tf.keras.datasets.mnist.load_data()
+for image in x_test:
+    image_data = image
+    image_data = image_data.astype('float32')/255
+    # Make shape 4D for Keras, (28,28,1) -> (1,28,28,1)
+    image_data = np.expand_dims(image_data, axis=0)
+    # Classify the input image
+    prediction = model.predict(image_data)
+    # Find the winner class and the probability
+    winner_class = np.argpartition(prediction, -1)[:, -1][0]
+    second_class = np.argpartition(prediction, -2)[:, -2][0]
+    third_class = np.argpartition(prediction, -3)[:, -3][0]
+
+    winner_probability = np.partition(prediction, -1)[:, -1][0]*100
+    second_probability = np.partition(prediction, -2)[:, -2][0]*100
+    third_probability = np.partition(prediction, -3)[:, -3][0]*100
+
+    # Build the text label
+    label = "klasa = {}, p = {:.2f}%".format(winner_class, winner_probability)
+    label2 = "klasa = {}, p = {:.2f}%".format(second_class, second_probability)
+    label3 = "klasa = {}, p = {:.2f}%".format(third_class, third_probability)
+
+    # Draw the label on the image
+    output_image = cv2.resize(image, (500,500))
+    cv2.putText(output_image, label, (10, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.75, 255, 2)
+    cv2.putText(output_image, label2, (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, 255, 2)
+    cv2.putText(output_image, label3, (10, 75), cv2.FONT_HERSHEY_SIMPLEX, 0.75, 255, 2)
+
+    # Show the output image        
+    cv2.imshow("Output", output_image)
+    cv2.waitKey(0)
---
+++ train.py
@@ -0,0 +1,74 @@
+﻿import os
+from matplotlib import pyplot as plt
+# Disable TF warning messages
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+import tensorflow as tf
+
+class FullyConnectedForMnist:
+    '''Simple NN for MNIST database. INPUT => FC/RELU => FC/SOFTMAX'''
+    def build(hidden_units):
+        # Initialize the model
+        model = tf.keras.models.Sequential()
+        # Flatten the input data of (x, y, 1) dimension
+        model.add(tf.keras.layers.Flatten(input_shape=(28,28,1)))
+        # FC/RELU layer
+        model.add(tf.keras.layers.Dense(hidden_units, activation='relu'))
+        # Softmax classifier (10 classes)
+        model.add(tf.keras.layers.Dense(10, activation="softmax"))
+        return model
+
+EPOCHS = 10
+MODEL_FILENAME = "model.h5"
+
+# Load dataset as train and test sets
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+
+# Convert from uint8 to float32 and normalize to [0,1]
+x_train = x_train.astype('float32')/255
+x_test = x_test.astype('float32')/255
+
+# Transform labels to 0-1 encoding, e.g.
+# 2 -> [0, 0, 1, 0, 0, 0, 0, 0, 0, 0]
+# 6 -> [0, 0, 0, 0, 0, 0, 1, 0, 0, 0]
+y_train = tf.keras.utils.to_categorical(y_train, 10)
+y_test = tf.keras.utils.to_categorical(y_test, 10)
+
+# Reshape the dataset into 4D array (required by Keras)
+# First dimension is the number of samples
+x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
+x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
+
+# Construct the model
+model = FullyConnectedForMnist.build(500)
+
+# Compile the model and print summary
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.summary()
+
+# Train the model
+history = model.fit(x=x_train, y=y_train, epochs=EPOCHS, validation_split=0.2)
+
+plt.title("accuracy by epochs")
+plt.ylabel("accuracy")
+plt.xlabel("epoch")
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.legend(['train split', 'validation split'])
+plt.show()
+
+plt.title("loss by epochs")
+plt.ylabel("loss")
+plt.xlabel("epoch")
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.legend(['train split', 'validation split'])
+plt.show()
+
+
+
+# Save model to a file
+model.save(MODEL_FILENAME)
+
+# Evaluate the model on the test data
+model.evaluate(x_test, y_test)
